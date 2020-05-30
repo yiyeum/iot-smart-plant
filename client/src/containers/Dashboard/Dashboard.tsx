838c321fcf0fc4b45083160daa52d8667bb91f9b
Kate YeEum Lee <yeeumm@gmail.com>
2020-05-30 02:35:40 -0400

    feat: create plant card

--- client/src/containers/Dashboard/Dashboard.tsx
+++ client/src/containers/Dashboard/Dashboard.tsx
@@ -1,19 +1,76 @@
 import React from 'react'
-import { Typography, Box } from '@material-ui/core'
+import { Grid, Typography, Box, WithStyles, withStyles, Card, CardActionArea, CardMedia, CardActions, CardContent, Button } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 import { useIntl } from 'react-intl'
-import { PLANT_DETAIL_URL } from '../../constants';
+import LocalDrinkRoundedIcon from '@material-ui/icons/LocalDrinkRounded'
+import Brightness4Icon from '@material-ui/icons/Brightness4'
+import { PLANT_DETAIL_URL } from '../../constants'
 
-export const Dashboard = () => {
+//                    <Link to={`${PLANT_DETAIL_URL}/${2}`}></Link>
+
+const styles = {
+    center: {
+        textAlign: 'center' as 'center',
+    },
+    action: {
+        margin: '0 auto'
+    },
+    icon: {
+        borderRadius: '50%',
+        backgroundColor: '#309ff5',
+        color: 'white',
+        width: '4em',
+        height: '4em',
+        textAlign: 'center' as 'center',
+        padding: '.9em',
+        display: 'inline-block',
+        marginRight: '2%'
+    },
+    light: {
+        backgroundColor: '#ff5a5a'
+    }
+}
+
+const DashboardBase = (props: WithStyles<typeof styles>) => {
+    const { classes } = props
     const { formatMessage } = useIntl()
     return (
         <Box p={6}>
             <Typography variant='h4'>{formatMessage({ id: 'dashboard.title' })}</Typography>
-            <ul>
-                <li>
-                    <Link to={`${PLANT_DETAIL_URL}/${2}`}></Link>
-                </li>
-            </ul>
+            <Grid container>
+                <Grid item md={4}>
+                    <Card>
+                        <CardActionArea>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    Name
+                                </Typography>
+                            </CardContent>
+                            <CardMedia
+                                image=""
+                                title="Contemplative Reptile"
+                            />
+                            <CardContent className={classes.center}>
+                                <div className={classes.icon}>
+                                    <LocalDrinkRoundedIcon />
+                                    <Typography variant='body2'>65%</Typography>
+                                </div>
+                                <div className={`${classes.icon} ${classes.light}`}>
+                                    <Brightness4Icon />
+                                    <Typography variant='body2'>70%</Typography>
+                                </div>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary" className={classes.action}>
+                                {formatMessage({ id: 'plant.card.water' })}
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+            </Grid>
         </Box>
     );
 }
+
+export const Dashboard = withStyles(styles)(DashboardBase)
